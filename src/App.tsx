ceffe778c34615a21e58bf9e25255750ef3678aa
Izumi Zama <dev@izumiz.me>
2022-04-20 03:07:47 +0900

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { Timer } from "./Timer";
+import useSound from "use-sound";
+import Sound from "./sound.mp3";
 
 function App() {
+  const [start, setStart] = useState<boolean>(false);
+  const [tick, setTick] = useState<number>(30 * 60);
+  const [play, { stop }] = useSound(Sound);
+
+  useEffect(() => {
+    if (start) {
+      const id = setInterval(() => {
+        setTick((t) => t - 1);
+      }, 1000);
+      if (tick === 0) {
+        play();
+      }
+      return () => clearInterval(id);
+    }
+  }, [tick, start, play]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ width: "100vw", height: "100vh" }}>
+      {/* <button onClick={() => play()}>SOUND TEST</button> */}
+      {/* <button onClick={() => stop()}>SOUND TEST</button> */}
+      <button onClick={() => setStart(true)}>START</button>
+      <button
+        onClick={() => {
+          setStart(false);
+          stop();
+        }}
+      >
+        STOP
+      </button>
+      <input
+        onChange={(e) => {
+          setTick(Number(e.target.value) * 60);
+        }}
+      />
+      Mins
+      <Timer tick={tick} />
     </div>
   );
 }
