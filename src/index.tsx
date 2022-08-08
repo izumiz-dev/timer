c4491f24ffdc192f6d02fe1d49aec712ed3b2241
Izumi Zama <dev@izumiz.me>
2022-08-08 14:19:38 +0900

    test for pwa installability

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import React from "react";
+import * as serviceWorker from "./serviceWorker";
 import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(
@@ -16,3 +17,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+serviceWorker.register();
